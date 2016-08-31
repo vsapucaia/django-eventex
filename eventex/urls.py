7653cb0e9b90c04587fa2582c58335148c6f2788
vsapucaia <vsfortunato@gmail.com>
2016-08-31 07:57:27 -0300

    urls

--- eventex/urls.py
+++ eventex/urls.py
@@ -17,6 +17,7 @@ from django.conf.urls import url
 from django.contrib import admin
 from eventex.core.views import home
 
+
 urlpatterns = [
     url(r'^$', home),
     url(r'^admin/', admin.site.urls),
